53810852747f5d1c64dd909d2f8217ef544e20af
Roman <katyrinroman@mail.ru>
2021-09-19 01:04:55 +0700

    Created the layout of the home screen

--- app/src/main/java/com/katyrin/testronasit/MainActivity.kt
+++ app/src/main/java/com/katyrin/testronasit/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        savedInstanceState ?: replaceMainFragment()
+    }
+
+    private fun replaceMainFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment.newInstance())
+            .commit()
     }
 }
